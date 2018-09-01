2f364c0441aa4ac1af383ec11e3e386c05b62234
taku paku <takupaku@yahoo.comemailgit config --global user.name taku>
2018-09-02 00:31:36 +0600

    Initial commit

---
+++ app/src/main/java/co/example/takwa/webviewtest/MainActivity.java
@@ -0,0 +1,83 @@
+package co.example.takwa.webviewtest;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar progressBar;
+    ImageView imageView;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressbarId);
+        imageView = findViewById(R.id.imageviewId);
+        progressBar.setMax(100);
+
+
+        webView = findViewById(R.id.webviewId);
+        linearLayout = findViewById(R.id.layout);
+        /*
+        websetting websetting=webview.websetting();
+        websetting.setjavatscript
+         */
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                linearLayout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                linearLayout.setVisibility(View.VISIBLE);
+            }
+        });
+        webView.loadUrl("https://www.google.com");
+        webView.setWebChromeClient(new WebChromeClient() {
+            //cntr+o
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                setTitle(title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                imageView.setImageBitmap(icon);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+        }
+
+    }
+}

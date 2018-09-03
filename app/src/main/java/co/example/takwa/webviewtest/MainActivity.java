c19ab7c91887e55e8610e729d24dd1e3b55261d9
taku paku <takupaku@yahoo.comemailgit config --global user.name taku>
2018-09-03 15:45:58 +0600

    menu and refresh option added

--- app/src/main/java/co/example/takwa/webviewtest/MainActivity.java
+++ app/src/main/java/co/example/takwa/webviewtest/MainActivity.java
@@ -1,8 +1,13 @@
 package co.example.takwa.webviewtest;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -10,12 +15,16 @@ import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
     ProgressBar progressBar;
     ImageView imageView;
     LinearLayout linearLayout;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    String pageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webviewId);
         linearLayout = findViewById(R.id.layout);
+        swipeRefreshLayout = findViewById(R.id.srLayoutId);
         /*
         websetting websetting=webview.websetting();
         websetting.setjavatscript
@@ -40,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 linearLayout.setVisibility(View.GONE);
+                swipeRefreshLayout.setRefreshing(false);
+                pageUrl = url;
             }
 
             @Override
@@ -49,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         webView.loadUrl("https://www.google.com");
+        swipeRefreshLayout.setRefreshing(true);
         webView.setWebChromeClient(new WebChromeClient() {
             //cntr+o
             @Override
@@ -69,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setImageBitmap(icon);
             }
         });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
     }
 
     @Override
@@ -79,5 +99,48 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.goBackaId:
+                //go back
+                onBackPressed();
+                break;
+            case R.id.goForwordId:
+
+                if(webView.canGoForward()){
+                    webView.goForward();
+
+                }
+                //forword
+                break;
+            case R.id.aboutUsId:
+                Toast.makeText(this,"about us",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.shareId:
+                Intent intent= new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT,pageUrl);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"page url");
+                startActivity(Intent.createChooser(intent,"pls select any of one"));
+
+
+                Toast.makeText(this,"Share ",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exitId:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
